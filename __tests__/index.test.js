f9f1b8cbad1538515dbb631715d2a8916a4b56f9
Franco Ortega <francopdx@gmail.com>
2020-12-08 14:05:16 -0800

    add POST test for pizza endpoint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,41 @@
+const fs = require('fs');
 const request = require('supertest');
 const app = require('../index.js');
+const pool = require('../lib/utils/pool.js');
 
 describe('app endpoints in index.js', () => {
-  it('responds with Heellloooo to test if things are connected correctly', () => {
-    return request(app)
-      .get('/')
-      .then(res => {
-        expect(res.text).toEqual('Heellloooo');
-      });
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  //   it('responds with Heellloooo to test if things are connected correctly', () => {
+  //     return request(app)
+  //       .get('/')
+  //       .then(res => {
+  //         expect(res.text).toEqual('Heellloooo');
+  //       });
+  //   });
+
+  it('creates a pizza via POST', async() => {
+    const response = await request(app)
+      .post('/pizza')
+      .send({
+        title: 'veggie',
+        toppings: 'mushroom, black olive, artichoke',
+        sauce: 'marinara',
+        price: '13'
+      });
+    expect(response.body).toEqual({
+      id: '1',
+      title: 'veggie',
+      toppings: 'mushroom, black olive, artichoke',
+      sauce: 'marinara',
+      price: '13'
+    });
+  });
+
 });
